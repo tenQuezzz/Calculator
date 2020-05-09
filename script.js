f63d6742d29cc63612a24a6cebec1f11000c52e0
Artyom Balashov <ten.que@yandex.ru>
2020-05-09 21:49:07 +0300

    Implement postfix expression calculation

--- script.js
+++ script.js
@@ -29,9 +29,37 @@ function convertFromInfixToPosfix(expression) {
   return result.trim();
 }
 
-console.log(convertFromInfixToPosfix('2 * 3 / 5'));
-// 100 100 + 1 - 2 + 3 + 4 5 * 6 / -
-
 function evaluatePosfixExpression(expression) {
+  if (expression.length == 0) {
+    return 0;
+  }
+  expression = expression.replace(/\s+/g, " ");
+  let opStack = [];
+  for (let item of expression.split(" ")) {
+    if (item.match(/\d/)) {
+      opStack.push(Number(item));
+    } else {
+      // extract two digits
+      let first = opStack.pop();
+      let second = opStack.pop();
+      if (!first || !second) return 'Error';
+      let res = operate(item, first, second);
+      if (res) {
+        opStack.push(res);
+        continue;
+      }
+      return 'Error';
+    }
+  }
+  return opStack.pop();
+}
 
+function operate(operation, lhs, rhs) {
+  switch (operation) {
+    case "+": return lhs + rhs;
+    case "-": return lhs - rhs;
+    case "*": return lhs * rhs;
+    case "/": return lhs / rhs;
+    default: return undefined;
+  }
 }
